e8f31b295703f3b9927c17589a963f29ce01a26a
SHIVANI MAHAJAN <122540794+shivanimahajan2626@users.noreply.github.com>
2026-05-21 01:32:24 +0530

    Add ProductList component to display plant products

---
+++ src/components/ProductList.jsx
@@ -0,0 +1,12 @@
+function ProductList() {
+  return (
+    <div>
+      <h2>Plant Products</h2>
+      <p>Snake Plant</p>
+      <p>Aloe Vera</p>
+      <p>Peace Lily</p>
+    </div>
+  );
+}
+
+export default ProductList;
