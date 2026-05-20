0d786924464ee55d388324f9c1520f624cd7f2ea
SHIVANI MAHAJAN <122540794+shivanimahajan2626@users.noreply.github.com>
2026-05-21 01:30:49 +0530

    Add AboutUs component for nursery information

---
+++ src/components/AboutUs.jsx
@@ -0,0 +1,10 @@
+function AboutUs() {
+  return (
+    <div>
+      <h2>About Paradise Nursery</h2>
+      <p>We provide beautiful indoor and outdoor plants.</p>
+    </div>
+  );
+}
+
+export default AboutUs;
