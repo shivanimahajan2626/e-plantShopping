972988578c87d06144cf741f8542723e82e4c767
SHIVANI MAHAJAN <122540794+shivanimahajan2626@users.noreply.github.com>
2026-05-21 01:54:59 +0530

    Refactor ProductList to display a list of plants

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,10 +1,25 @@
+const plants = [
+  { id: 1, name: "Rose", price: 10, image: "rose.jpg" },
+  { id: 2, name: "Tulip", price: 15, image: "tulip.jpg" },
+  { id: 3, name: "Lily", price: 20, image: "lily.jpg" },
+  { id: 4, name: "Aloe Vera", price: 12, image: "aloe.jpg" },
+  { id: 5, name: "Snake Plant", price: 18, image: "snake.jpg" },
+  { id: 6, name: "Money Plant", price: 25, image: "money.jpg" },
+];
+
 function ProductList() {
   return (
     <div>
-      <h2>Plant Products</h2>
-      <p>Snake Plant</p>
-      <p>Aloe Vera</p>
-      <p>Peace Lily</p>
+      <h1>Plant List</h1>
+
+      {plants.map((plant) => (
+        <div key={plant.id}>
+          <img src={plant.image} alt={plant.name} width="100" />
+          <h3>{plant.name}</h3>
+          <p>${plant.price}</p>
+          <button>Add to Cart</button>
+        </div>
+      ))}
     </div>
   );
 }
